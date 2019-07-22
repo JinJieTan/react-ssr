33bafeca2db3c57a91e3113b43e730dfbf989382
tanjinjie <a@PeterdeMacBook-Pro.local>
2019-07-22 21:58:29 +0800

    ssr

---
+++ webpack.server.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const merge = require('webpack-merge')
+const config = require('./webpack.base')
+
+const serverConfig = {
+  mode: 'development',
+  entry: './server/index.js',
+  externals: [nodeExternals()],
+  module: {
+    // rules: [{
+    //   test: /\.css?$/,
+    //   use: ['isomorphic-style-loader', {
+    //     loader: 'css-loader',
+    //     options: {
+    //       modules: true
+    //     }
+    //   }]
+    // }]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+}
+
+module.exports = merge(config, serverConfig)
